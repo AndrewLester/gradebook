475e8eb6abdc91a7e24f9150bca94b588d81f5fc
AndrewLester <alester220@gmail.com>
2022-09-10 02:30:52 -0500

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+import { page } from '$app/stores';
+</script>
+
+<h1>{$page.status}</h1>
+{#if $page.status === 404}
+	<h2>Page not found</h2>
+{:else}
+	<h2>An error occured</h2>
+{/if}
+<p><a href="/">Return home</a></p>
